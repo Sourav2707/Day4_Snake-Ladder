e56863e7ab8dd22833eac9a681d1523b37458355
Sourav2707 <smartsourav33@gmail.com>
2022-12-14 16:20:24 +0530

    Initial commit with welcome message

--- src/com/snake_ladder/SnakeAndLadder.java
+++ src/com/snake_ladder/SnakeAndLadder.java
@@ -1,6 +1,6 @@
 package com.snake_ladder;
 
-public class Start {
+public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to day 4 assignment problem Snake and Ladder");
     }
