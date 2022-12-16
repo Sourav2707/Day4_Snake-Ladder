dbdba4ff08fa95f5bd8080d5337e60880aabf5fa
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 16:59:49 +0530

    UC4 playing till the player reaches 100th position

--- src/com/day4_snake_ladder/SnakeLadder.java
+++ src/com/day4_snake_ladder/SnakeLadder.java
@@ -4,38 +4,37 @@ public class SnakeLadder {
     static final int startPosition = 0;
     static final int ladderOption = 2;
     static final int snakeOption = 1;
+    static int position;
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder program");
-        Random rnd = new Random();
-        int dieNumber, option;
-        int position = startPosition;
+        position = startPosition;
+        do {
+            int dieNumber, option;
+            Random rnd = new Random();
+            dieNumber = rnd.nextInt(1, 7);
+            option = rnd.nextInt(1, 3);
+            System.out.println("Die Number = " + dieNumber);
 
-        dieNumber = rnd.nextInt(1,7);
-        option = rnd.nextInt(1,3);
-        System.out.println("Die Number = "+dieNumber);
-
-        switch (option){
-            case ladderOption:
-                System.out.println("ladder option");
-                position += dieNumber;
-                break;
-            case snakeOption:
-                System.out.println("snake option");
-                position -= dieNumber;
-                if (position < 0)
-                {
-                    System.out.println("The player bit by snake");
-                    position = 0;
-                }
-                else
-                {
-                    System.out.println("The player bit by snake");
-                }
-                break;
-            default:
-                System.out.println("No Play option");
-                break;
-        }
-        System.out.println("current position : "+position);
+            switch (option) {
+                case ladderOption:
+                    System.out.println("ladder option");
+                    position += dieNumber;
+                    break;
+                case snakeOption:
+                    System.out.println("snake option");
+                    position -= dieNumber;
+                    if (position < 0) {
+                        System.out.println("The player bit by snake");
+                        position = 0;
+                    } else {
+                        System.out.println("The player bit by snake");
+                    }
+                    break;
+                default:
+                    System.out.println("No Play option");
+                    break;
+            }
+            System.out.println("current position : " + position+"\n");
+        }while (position < 100);
     }
 }
