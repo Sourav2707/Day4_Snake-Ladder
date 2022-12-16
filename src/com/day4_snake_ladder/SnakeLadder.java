3841aa05fd181fab6082a1db3d9fe9978cedcca0
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 16:55:03 +0530

    UC2 player rolling die from position 0

--- src/com/day4_snake_ladder/SnakeLadder.java
+++ src/com/day4_snake_ladder/SnakeLadder.java
@@ -1,10 +1,15 @@
 package com.day4_snake_ladder;
-
+import java.util.Random;
 public class SnakeLadder {
     static final int startPosition = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder program");
-        int position = startPosition;
+        int dieNumber;
+        int position =startPosition;
+        Random rnd = new Random();
         System.out.println("current position : "+position);
+        dieNumber = rnd.nextInt(1,7);
+
+        System.out.println("Die Number = "+dieNumber);
     }
 }
