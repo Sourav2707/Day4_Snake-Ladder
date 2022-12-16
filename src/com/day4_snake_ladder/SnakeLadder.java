8723a997799c3436f50c87ceea758ec7fd168688
Sourav2707 <smartsourav33@gmail.com>
2022-12-16 16:51:09 +0530

    UC1 starting with single player in position 0

--- src/com/day4_snake_ladder/SnakeLadder.java
+++ src/com/day4_snake_ladder/SnakeLadder.java
@@ -1,7 +1,10 @@
 package com.day4_snake_ladder;
 
 public class SnakeLadder {
+    static final int startPosition = 0;
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder program");
+        int position = startPosition;
+        System.out.println("current position : "+position);
     }
 }
